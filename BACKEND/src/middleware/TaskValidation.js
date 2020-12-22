1dcf8cf5148d47743a51222ab3aa918b1a13bb57
GabrielFelix23 <srgfelix123@gmail.com>
2020-12-22 15:31:33 -0300

    Atualizando uma Tarefa

--- BACKEND/src/middleware/TaskValidation.js
+++ BACKEND/src/middleware/TaskValidation.js
@@ -1,5 +1,5 @@
 const TaskModel = require('../model/TaskModel')
-const { isPast} = require("date-fns")
+const { isPast } = require("date-fns")
 
 const TaskValidation = async (req, res, next) => {
     const { macaddress, type, title, description, when } = req.body
@@ -25,9 +25,20 @@ const TaskValidation = async (req, res, next) => {
     else{
         let exists
 
-        exists = await TaskModel.findOne({'when': {'$eq': new Date(when)},
+        if(req.params.id){
+            exists = await TaskModel.findOne({
+                                        '_id': {'$ne': req.params.id},
+                                        'when': {'$eq': new Date(when)},
                                         'macaddress': {'$in': macaddress}
                                     })
+        }else{
+            exists = await TaskModel.findOne({
+                                        'when': {'$eq': new Date(when)},
+                                        'macaddress': {'$in': macaddress}
+            })
+        }
+
+        
         if(exists){
             return res.status(400).json({error: "Já existe uma tarefa nesse dia e horário."})
         }
