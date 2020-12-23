d64037ffbad68e60e0c9c3b860afa7312b242ecb
GabrielFelix23 <srgfelix123@gmail.com>
2020-12-23 15:21:21 -0300

    Listando todas as tarefas

--- BACKEND/src/routes/TaskRoutes.js
+++ BACKEND/src/routes/TaskRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const TaskController = require('../controller/TaskController')
 const TaskValidation = require('../middleware/TaskValidation')
+const macaddressValidation = require('../middleware/MacaddressValidation')
 
 router.post('/', TaskValidation,  TaskController.create)
 router.put('/:id', TaskValidation, TaskController.update)
+router.get('/filter/all', macaddressValidation, TaskController.all)
 
 module.exports = router
